95cc7deffc91d86dfd0d4db8b3dbf32e3e6d98b3
benhastings <ben.hastings@gmail.com>
2013-08-15 15:48:53 -0400

    Update phantomArticles.py

--- phantomArticles.py
+++ phantomArticles.py
@@ -1,28 +1,28 @@
-from subprocess import Popen
-import sys
-import urllib2
+import subprocess
 import time
-# Find hostname to use for passing to webdriver
-resp=urllib2.urlopen('http://169.254.169.254/latest/meta-data/public-hostname')
-PHOST=resp.read()
+import datetime
+import csv
+import random
+import sys
 
-# Poll Hub interface to determine free/busy status of resources
-def freeCheck():
-        try:
-                response=urllib2.urlopen('http://localhost:4200/grid/console')
-                html=response.read()
-                exit
-        except urllib2.URLError:
-                pass
 
-        count = html.count("platform=")
-        return count
+#------------------------------------------------------------
+#--- Get Interactive Input for number of loops to execute ---
+numLoops = int(sys.argv[1])
+baseURL = sys.argv[2]
+thread = sys.argv[3]
+#--- Read List of PIIs -----------------
+PII=[]
+csvRd = csv.reader(open('piis3k.csv','rb'))
+for j in csvRd:
+        PII.append(j)
 
-# If resources available (first condition) add more requests
-#   - rename whatever test you want to run to the 'sdtest.py' value
-# Otherwise, exit and wait a while
-#if 'type=WebDriver' in html:
-freeCount=freeCheck()
-while freeCount>0:
-        if freeCount < 4:
-                Popen('python gridExecute.py '+str(idx)+' 0 sdtest.py 4000000 '+PHOST+'&',shell=True)
+ct=0
+while(ct < numLoops):
+        idx=int(random.random()*3100)
+        #idx=ct
+        #Popen('phantomjs get.js www.sciencedirect.com S0044848600005160+'&',shell=True)
+        #Popen('phantomjs get.js '+baseURL+' '+PII[idx]+'&',shell=True)
+        out=subprocess.check_output('phantomjs get.js '+baseURL+' '+str(PII[idx]).strip('[\']'),shell=True)
+        print(thread+'\t'+out)  
+        ct=ct+1
